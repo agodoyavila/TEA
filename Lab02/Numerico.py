8a03ec0a2b622f7d90a77f9b18a030ca18371726
agodoyavila <114714967+agodoyavila@users.noreply.github.com>
2022-09-30 00:32:08 -0600

    Add files via upload

---
+++ Lab02/Numerico.py
@@ -0,0 +1,19 @@
+#Tendecias e Innovacion en Tecnologia Agricola (Tea)
+from re import X
+
+
+x =20
+
+#display x:
+print(x)
+
+#disllay the data type of x:
+print(type(x))
+
+y=20.5
+
+#display y:
+print(y)
+
+#display de data type of y:
+print(type(y))
---
+++ Lab02/celsius2farenheit.py
@@ -0,0 +1,4 @@
+# Tendencias e Innovacion en Tecnologia Agricola (TEA)
+tempCelsius= input("Escriba una temperatura en celcius=")
+tempFarenheit= int (tempCelsius) * 1.8  + 32
+print ("La temperatura en Farenheit es:"+ str (tempFarenheit))
